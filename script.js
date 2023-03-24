d76907d65e5039e9c0ccadd003aff3e5b8cb4903
MistrWarrior <125338357+MistrWarrior@users.noreply.github.com>
2023-03-23 22:41:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function multiplicar() {
+var numero1 = document.getElementById("numero1").value;
+var numero2 = document.getElementById("numero2").value;
+var resultado = numero1 * numero2;
+alert(resultado);
+
+}
+
